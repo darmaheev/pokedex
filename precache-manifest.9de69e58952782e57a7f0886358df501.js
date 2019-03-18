711616467e984ef8099b47037bfff5c9acad550f
V.Darmakheev <v.darmakheev@playa.ru>
2019-03-18 12:31:36 +0800

    Updates

---
+++ precache-manifest.9de69e58952782e57a7f0886358df501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "21062b874cb25d95f14e3b228858fa67",
+    "url": "/pokedex/static/media/pokedex.21062b87.png"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "379da9dc034952787429",
+    "url": "/pokedex/static/js/main.c8a8aeaf.chunk.js"
+  },
+  {
+    "revision": "4cd9536996dcba0ae7aa",
+    "url": "/pokedex/static/js/2.baf58367.chunk.js"
+  },
+  {
+    "revision": "379da9dc034952787429",
+    "url": "/pokedex/static/css/main.6b9aeccd.chunk.css"
+  },
+  {
+    "revision": "4cd9536996dcba0ae7aa",
+    "url": "/pokedex/static/css/2.42782791.chunk.css"
+  },
+  {
+    "revision": "8b62bc35e1c32ac5d2b84c416c530de2",
+    "url": "/pokedex/index.html"
+  }
+];
